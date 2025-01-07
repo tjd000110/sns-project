657bc7424e5bf9ef8a925f261b9738e6326f84a1
misung choi <tjd000110@gmail.com>
2025-01-07 21:43:16 +0900

    post페이지 ui 생성

--- src/routes/posts.router.js
+++ src/routes/posts.router.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const { checkAuthenticated } = require('../middlewares/auth');
 const router = express.Router();
+const Post = require('../models/posts.model');
+const Comment = require('../models/comments.model');
+
 
 router.get('/', checkAuthenticated, (req, res) => {
-    res.render('posts');
+
+    Post.find()
+    .populate('comments')
+    .sort({ createdAt: -1 })
+    .exec((err, posts) => {
+        if(err) {
+            console.log(err);
+        } else{
+
+            res.render('posts', {
+                posts: posts,
+                currentUser: req.user
+            });
+            
+        }
+    })
+    
 })
 
 module.exports = router;
